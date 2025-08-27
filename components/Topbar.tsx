ac926664fe54783733681597deb5ab2df9fee1d2
liuxuedata <liuxuedata@gmail.com>
2025-08-27 14:00:03 +0800

    Add logo and favicon

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default function Topbar() {
   return (
     <header className="h-12 border-b border-[var(--border)] bg-[var(--background)] flex items-center px-4">
+      <Image src="/logo.svg" alt="logo" width={28} height={28} className="mr-2" />
       <span className="font-medium">选品平台</span>
     </header>
   );
