68e2169ce964d98326d506c6b4a9a8d445c0f760
liuxuedata <liuxuedata@gmail.com>
2025-08-28 22:25:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     serverComponentsExternalPackages: ['pg']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'pg', 'pg-native'];
+    }
+    return config;
   }
 };
-
 module.exports = nextConfig;
