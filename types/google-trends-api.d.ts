dadbe77e152bd3fbb1cbf4af7d5855f1e4c90463
liuxuedata <liuxuedata@gmail.com>
2025-08-31 00:23:12 +0800

    Update google-trends-api.d.ts

--- types/google-trends-api.d.ts
+++ types/google-trends-api.d.ts
@@ -1,9 +1,8 @@
- 
+
 declare module 'google-trends-api' {
-  export function interestOverTime(params: any): Promise<any>;
-  export function relatedQueries(params: any): Promise<any>;
-   export function trendingSearches(params: { geo: string }): Promise<string>;
   export function interestOverTime(params: { keyword: string, startTime: Date, endTime: Date, geo: string }): Promise<any>;
-}  export function interestByRegion(params: any): Promise<any>;
+  export function relatedQueries(params: any): Promise<any>;
+  export function trendingSearches(params: { geo: string }): Promise<string>;
+  export function interestByRegion(params: any): Promise<any>;
   export function interestByCity(params: any): Promise<any>;
 }
